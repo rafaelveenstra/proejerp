c1c1d8fccd69ead73a57c321b64d3c1fcb6fc1ce
rafael veenstra <rafaelveenstra@gmail.com>
2020-02-23 21:26:09 -0300

    inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# Rails.application.routes.draw do
+#   get 'pages/home'
+#   get 'pages/contact'
+#   get 'pages/about'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'home', to: 'pages#home', as: :home
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
 end
